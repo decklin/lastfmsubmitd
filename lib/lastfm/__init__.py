3725be84d56ca51fd9a357d64c4443479e46df64
Decklin Foster <decklin@red-bean.com>
2005-12-08 05:27:03 +0000

    It compiles, import it!

---
+++ lib/lastfm/__init__.py
@@ -0,0 +1,19 @@
+__all__ = ['asyaml', 'logger']
+
+# Our IPC mechanisms.
+SUBMITD_CACHE = "/var/cache/lastfmsubmitd/subs"
+SUBMITD_FIFO = "/var/run/lastfmsubmitd/fifo"
+SUBMITD_PIDFILE = "/var/run/lastfmsubmitd/pid"
+
+# A song under this length will not be submitted.
+MIN_LEN = 30
+
+# A song over this length will not be submitted.
+MAX_LEN = 3600
+
+# A song will be submitted if it reaches this percentage...
+SUB_PERCENT = 0.5
+
+# ...or if it has been played for this many seconds.
+SUB_SECONDS = 240
+
---
+++ lib/lastfm/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+LASTFM_LOG = './lastfm.log' # '/var/log/lastfm.log'
+
+def create_log(name, debug=True):
+
+    if debug:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+
+    log = logging.getLogger(name)
+    log.setLevel(level)
+
+    format = logging.Formatter(
+        '%(asctime)s %(name)s[%(process)s] %(levelname)s: %(message)s')
+
+    logfile = logging.FileHandler(LASTFM_LOG)
+    logfile.setLevel(level)
+    logfile.setFormatter(format)
+    log.addHandler(logfile)
+
+    return log
