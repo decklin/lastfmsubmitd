a77d1fc7a903bbe415fb13c7c91a1d3d85eaf082
Decklin Foster <decklin@red-bean.com>
2006-01-03 06:11:03 +0000

    And a setup script to go with it.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# If you're installing by hand, run this as root, and run the daemons from the
+# following user accounts.
+
+addgroup lastfm
+adduser --ingroup lastfm lastfmsubmitd
+adduser --ingroup lastfm lastmp
+
+RUN=/var/run/lastfmsubmitd
+CACHE=/var/cache/lastfmsubmitd
+
+mkdir $RUN
+mkfifo $RUN/fifo
+chown lastfmsubmitd:lastmp $RUN/fifo
+chmod 660 $RUN/fifo
+
+mkdir $CACHE
+touch $CACHE/subs
+chown lastfmsubmitd:lastmp $CACHE/subs
+chmod 660 $CACHE/subs
