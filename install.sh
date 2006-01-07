7d9116d73d404d5e859758749b90a2b142e93e3c
Decklin Foster <decklin@red-bean.com>
2006-01-07 22:23:02 +0000

    Install fixes suggested by Andrew Price.

--- install.sh
+++ install.sh
@@ -3,19 +3,32 @@
 # If you're installing by hand, run this as root, and run the daemons from the
 # following user accounts.
 
+./setup.py install
+
 groupadd lastfm
 useradd -g lastfm lastfmsubmitd
 useradd -g lastfm lastmp
 
 RUN=/var/run/lastfmsubmitd
 CACHE=/var/cache/lastfmsubmitd
+LOG=/var/log/lastfm.log
+
+touch $LOG
+chown 664 $LOG
+chgrp lastfm $LOG
 
 mkdir $RUN
+chgrp lastfm $RUN
+chmod 775 $RUN
+
 mkfifo $RUN/fifo
-chown lastfmsubmitd:lastfm $RUN/fifo
+chgrp lastfm $RUN/fifo
 chmod 660 $RUN/fifo
 
 mkdir $CACHE
+chgrp lastfm $CACHE
+chmod 775 $CACHE
+
 touch $CACHE/subs
-chown lastfmsubmitd:lastfm $CACHE/subs
+chgrp lastfm $CACHE/subs
 chmod 660 $CACHE/subs
