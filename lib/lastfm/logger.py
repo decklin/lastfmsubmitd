204593e646860a318d3365afe917509f50ec4a3b
Decklin Foster <decklin@red-bean.com>
2006-02-06 02:32:01 +0000

    Don't replace decode errors in lastfmsubmit. Try utf-8, then the locale's encoding or whatever was specified on the command line. Use the logger to print to stderr as well as the log.

--- lib/lastfm/logger.py
+++ lib/lastfm/logger.py
@@ -1,23 +1,28 @@
 import os
+import sys
 import logging
 
-def getlog(name, logfile, debug=False):
-    if debug:
-        level = logging.DEBUG
-    else:
-        level = logging.INFO
+def getlog(name, logfile, debug=False, stderr=False):
+    if debug: level = logging.DEBUG
+    else: level = logging.INFO
 
     logger = logging.getLogger(name)
     logger.setLevel(level)
 
-    formatter = logging.Formatter(
-        '%(asctime)s %(name)s[%(process)s] %(levelname)s: %(message)s')
-
     os.umask(002)
-    loghandler = logging.FileHandler(logfile)
-    loghandler.setLevel(level)
-    loghandler.setFormatter(formatter)
-    logger.addHandler(loghandler)
+
+    filefmt = '%(asctime)s %(name)s[%(process)s] %(levelname)s: %(message)s'
+    filehandler = logging.FileHandler(logfile)
+    filehandler.setLevel(level)
+    filehandler.setFormatter(logging.Formatter(filefmt))
+    logger.addHandler(filehandler)
+
+    if stderr:
+        stderrfmt = '%(levelname)s: %(message)s'
+        stderrhandler = logging.StreamHandler(sys.stderr)
+        stderrhandler.setLevel(level)
+        stderrhandler.setFormatter(logging.Formatter(stderrfmt))
+        logger.addHandler(stderrhandler)
 
     return logger
 
