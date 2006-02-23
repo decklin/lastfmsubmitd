685f1fc2fda37e54f14b3dd18d91f16a6c0aa11e
Decklin Foster <decklin@red-bean.com>
2006-02-23 19:44:01 +0000

    Release 0.22. I am NOT GOING TO TOUCH EXPORT WITHOUT PLAYING A WHOLE ALBUM NEXT TIME.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='lastfmsubmitd',
-    version='0.21',
+    version='0.22',
     description='Last.fm submit daemon and example clients for MPD/MusicBrainz',
     author='Decklin Foster',
     author_email='decklin@red-bean.com',
