6986dd3f8c291132b1ab33bd052150ec36af695a
Decklin Foster <decklin@red-bean.com>
2006-03-24 10:53:05 +0000

    Release 0.25.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='lastfmsubmitd',
-    version='0.24',
+    version='0.25',
     description='Last.fm submit daemon and example clients for MPD/MusicBrainz',
     author='Decklin Foster',
     author_email='decklin@red-bean.com',
