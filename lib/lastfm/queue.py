3ecb24df189c5c34b67c0fab375d30e9cfa8ba43
Decklin Foster <decklin@red-bean.com>
2006-03-25 18:39:01 +0000

    Make names consistent within here. Need to reconcile the rest of the world. Don't we all.

--- lib/lastfm/queue.py
+++ lib/lastfm/queue.py
@@ -7,20 +7,20 @@ class Reader:
     """Reads one writer at a time from our fifo, making sure it actually
     exists first."""
 
-    def __init__(self, log, f):
+    def __init__(self, log, fname):
         self.log = log
-        self.f = f
+        self.fname = fname
         try:
-            if stat.S_ISFIFO(os.stat(self.f).st_mode): return
+            if stat.S_ISFIFO(os.stat(self.fname).st_mode): return
         except OSError, e:
             try:
-                os.mkfifo(self.f)
+                os.mkfifo(self.fname)
             except OSError, e:
                 self.log.error('Failed to create %s: %s' % (f, e))
                 raise
 
     def select(self, timeout):
-        fd = os.open(self.f, os.O_NONBLOCK)
+        fd = os.open(self.fname, os.O_NONBLOCK)
         rx, wx, ex = select.select([fd], [], [], timeout)
         data = []
         if fd in rx:
@@ -38,25 +38,27 @@ class Reader:
 class Writer:
     """Writes data to the fifo, or just a plain file."""
 
-    def __init__(self, log, f, l):
+    def __init__(self, log, fname, lockname):
         self.log = log
-        self.outfile = f
-        self.lock = l
+        self.fname = fname
+        self.lockname = lockname
 
-        if self.lock:
+        if self.lockname:
             self.log.debug('Requesting lock on %s' % self.lock)
             self.lock = file(self.lock, 'w')
+            # If there is a pileup, locks will be requested in order.
+            fcntl.flock(self.lock, fcntl.LOCK_EX)
+        else:
+            self.lock = None
 
-        # If there is a pileup, locks will be requested in order.
-        fcntl.flock(self.lock, fcntl.LOCK_EX)
         # This will also block, until we have a reader.
-        self.log.debug('Opening %s' % self.outfile)
-        self.out = file(self.outfile, 'w')
+        self.log.debug('Opening %s' % self.fname)
+        self.f = file(self.fname, 'w')
 
     def __del__(self):
+        # We must flush and get out of the way first.
+        self.f.close()
         if self.lock:
-            # We must flush and get out of the way first.
-            self.out.close()
             fcntl.flock(self.lock, fcntl.LOCK_UN)
             self.lock.close()
             self.log.debug('Released lock')
